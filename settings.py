b706824dd0c927ea41f631c795d37acc54be2da9
PacersCyc <452720855@qq.com>
2022-09-25 13:08:04 +0800

    feat: add ship

---
+++ settings.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+class Settings():
+  def __init__(self):
+    self.screen_width = 1200
+    self.screen_height = 800
+    self.bg_color = (230, 230, 230)
