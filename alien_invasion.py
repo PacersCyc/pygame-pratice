a195aa1cd0ef5fefdf2bfef2ef30abb2a9283422
PacersCyc <452720855@qq.com>
2022-09-25 19:02:13 +0800

    feat: add score and level

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from ship import Ship
 from alien import Alien
 from game_stats import GameStats
 from button import Button
+from scoreboard import ScoreBoard
 import game_functions as gf
 
 def run_game():
@@ -15,6 +16,7 @@ def run_game():
   pygame.display.set_caption("Alien Invasion")
   play_button = Button(ai_settings, screen, "Play")
   stats = GameStats(ai_settings)
+  sb = ScoreBoard(ai_settings, screen, stats)
   ship = Ship(ai_settings, screen)
   bullets = Group()
   aliens = Group()
@@ -22,12 +24,12 @@ def run_game():
   gf.create_fleet(ai_settings, screen, aliens, ship)
   
   while True:
-    gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+    gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
     if stats.game_active:
       ship.update()
-      gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-      gf.update_aliens(ai_settings, stats, screen, aliens, ship, bullets)
+      gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+      gf.update_aliens(ai_settings, stats, sb, screen, aliens, ship, bullets)
     
-    gf.update_screen(ai_settings, screen, stats, ship, bullets, aliens, play_button)
+    gf.update_screen(ai_settings, screen, stats, sb, ship, bullets, aliens, play_button)
     
 run_game()
