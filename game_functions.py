ba5dcf5df3ac162072b360414f405abdda489591
PacersCyc <452720855@qq.com>
2022-09-25 17:59:02 +0800

    feat: speed increase

--- game_functions.py
+++ game_functions.py
@@ -43,6 +43,9 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
   button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
   if button_clicked and not stats.game_active:
+    # 重置游戏速度
+    ai_settings.initialize_dynamic_settings()
+    
     pygame.mouse.set_visible(False)
     stats.reset_stats()
     stats.game_active = True
@@ -71,6 +74,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
   collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
   if len(aliens) == 0:
     bullets.empty()
+    ai_settings.increase_speed()
     create_fleet(ai_settings, screen, aliens, ship)
   
 def update_bullets(ai_settings, screen, ship, aliens, bullets):
